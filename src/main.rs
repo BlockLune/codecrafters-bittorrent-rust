d6370fe72b4fa07851e9b97c925729a8b2fd0b49
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-08 11:02:11 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,52 @@
-use serde_json;
+use serde_bencode;
+use serde_json::{self, Value as JsonValue};
 use std::env;
+use std::str;
 
-fn decode_bencoded_integer(encoded_value: &str) -> Result<(serde_json::Value, usize), String> {
-    let err_message = format!("Failed to parse an integer from {}", encoded_value);
-
-    let begin_index = encoded_value.find("i").ok_or_else(|| err_message.clone())?;
-    if begin_index != 0 {
-        return Err(err_message.clone());
+fn bencode_to_json(value: &serde_bencode::value::Value) -> Result<JsonValue, String> {
+    match value {
+        serde_bencode::value::Value::Bytes(bytes) => match str::from_utf8(&bytes) {
+            Ok(s) => Ok(JsonValue::String(s.to_string())),
+            Err(e) => Err(format!("Error converting bytes: {}", e)),
+        },
+        serde_bencode::value::Value::Int(integer) => {
+            Ok(JsonValue::Number(serde_json::Number::from(*integer)))
+        }
+        serde_bencode::value::Value::List(list) => {
+            let json_list: Result<Vec<_>, _> = list.iter().map(bencode_to_json).collect();
+            json_list.map(JsonValue::Array)
+        }
+        serde_bencode::value::Value::Dict(dict) => {
+            let mut json_map = serde_json::Map::new();
+            for (key, value) in dict {
+                match str::from_utf8(&key) {
+                    Ok(key_str) => {
+                        let json_value = bencode_to_json(value)?;
+                        json_map.insert(key_str.to_string(), json_value);
+                    }
+                    Err(e) => return Err(format!("Error converting key bytes: {}", e)),
+                }
+            }
+            Ok(JsonValue::Object(json_map))
+        }
     }
-
-    let end_index = encoded_value.find('e').ok_or_else(|| err_message.clone())?;
-    let int_str = &encoded_value[1..end_index];
-
-    int_str
-        .parse::<i64>()
-        .map(|integer| {
-            (
-                serde_json::Value::Number(serde_json::Number::from(integer)),
-                end_index,
-            )
-        })
-        .map_err(|_| err_message)
 }
 
-fn decode_bencoded_string(encoded_value: &str) -> Result<(serde_json::Value, usize), String> {
-    let err_message = format!("Failed to parse a string from {}", encoded_value);
-
-    if !encoded_value.chars().next().unwrap().is_digit(10) {
-        return Err(err_message.clone());
-    }
-
-    let colon_index = encoded_value.find(':').ok_or_else(|| err_message.clone())?;
-
-    let number_str = &encoded_value[..colon_index];
-    let number = number_str.parse::<i64>().map_err(|_| err_message.clone())?;
-    let end_index = colon_index + 1 + number as usize;
-    let string = &encoded_value[colon_index + 1..end_index];
-    Ok((serde_json::Value::String(string.to_string()), end_index))
-}
-
-fn decode_bencoded_list(encoded_value: &str) -> Result<(serde_json::Value, usize), String> {
-    let err_message = format!("Failed to parse a list from {}", encoded_value);
-
-    if encoded_value.chars().next().unwrap() != 'l' {
-        return Err(err_message.clone());
-    }
-
-    todo!()
-}
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        return decode_bencoded_string(encoded_value).unwrap().0;
-    } else if encoded_value.starts_with("i") {
-        return decode_bencoded_integer(encoded_value).unwrap().0;
-    } else if encoded_value.starts_with("l") {
-    }
-    panic!("Unhandled encoded value: {}", encoded_value)
-}
-
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        match serde_bencode::from_str::<serde_bencode::value::Value>(&encoded_value) {
+            Ok(value) => match bencode_to_json(&value) {
+                Ok(json_value) => {
+                    println!("{}", json_value)
+                }
+                Err(e) => eprintln!("{}", e),
+            },
+            Err(e) => eprintln!("{}", e),
+        }
     } else {
         println!("unknown command: {}", args[1])
     }
